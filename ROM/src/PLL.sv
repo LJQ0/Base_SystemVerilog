8d64046bd0c6ffc6b20ba5ab602b170efffd10c9
221220000-Liu Jiaqi <ljq1019395070@163.com>
2024-03-03 17:42:18 +0800

    this is real start

--- ROM/src/PLL.sv
+++
@@ -1,7 +0,0 @@
-module PLL#(
-    parameter int HDPLL_CLK_KHZ_IN      = 50_000,
-    parameter int HDPLL_PIXEL_KHZ_OUT   = 27_000,
-    parameter bit HDPLL_USE_2_PLLS      = 1'b1,
-    parameter int HDPLL_TRICK_MBPS      = 0,
-    
-
