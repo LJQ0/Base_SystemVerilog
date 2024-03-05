882e7beb5f9ad89bdcf2a86b9c8122ea83092681
221220000-Liu Jiaqi <ljq1019395070@163.com>
2024-03-05 08:34:30 +0800

    is same as the up

--- DDS/sims/verdiLog/verdi.cmd
+++ DDS/sims/verdiLog/verdi.cmd
@@ -2,64 +2,37 @@ debImport "-f" "filelist.f"
 debLoadSimResult /home/host/.ssh/base/DDS/sims/DDS_tb.fsdb
 wvCreateWindow
 srcHBSelect "DDS_tb.DDS_inst" -win $_nTrace1
-srcHBAddObjectToWave -clipboard
-wvDrop -win $_nWave2
-wvSetCursor -win $_nWave2 1430283.510458 -snap {("G2" 0)}
-wvZoomAll -win $_nWave2
-wvSelectSignal -win $_nWave2 {( "DDS_inst" 5 )} 
-wvBusWaveform -win $_nWave2 -analog
-wvSetPosition -win $_nWave2 {("DDS_inst" 5)}
-wvZoom -win $_nWave2 10650111.062484 11530285.530458
-wvSetCursor -win $_nWave2 10750592.853595 -snap {("DDS_inst" 5)}
-wvZoom -win $_nWave2 11047052.489213 11052805.263514
-wvZoom -win $_nWave2 11049684.477452 11049747.143839
-wvSetCursor -win $_nWave2 11049713.830986 -snap {("DDS_inst" 1)}
-srcActiveTrace "DDS_tb.DDS_inst.clk" -win $_nTrace1 -TraceByDConWave -TraceTime \
-           11049710 -TraceValue 1
-wvZoom -win $_nWave2 11049713.557930 11049713.721764
-wvZoomIn -win $_nWave2
-wvZoomIn -win $_nWave2
-wvZoomIn -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoom -win $_nWave2 11058223.743333 11059822.878845
-wvZoom -win $_nWave2 11059036.201721 11059092.641798
-wvSetCursor -win $_nWave2 11059051.264701 -snap {("G2" 0)}
-wvZoomAll -win $_nWave2
-wvZoomIn -win $_nWave2
-wvZoomIn -win $_nWave2
-wvZoomIn -win $_nWave2
-wvZoomIn -win $_nWave2
-wvZoomIn -win $_nWave2
-wvZoomIn -win $_nWave2
-wvZoomIn -win $_nWave2
-wvZoomIn -win $_nWave2
-wvZoomIn -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvZoomOut -win $_nWave2
-wvSetCursor -win $_nWave2 123229.872141 -snap {("DDS_inst" 5)}
-wvSetMarker -win $_nWave2 30.000000
-wvSetMarker -win $_nWave2 245630.000000
-wvSetMarker -win $_nWave2 368510.000000
-wvSetCursor -win $_nWave2 249183.056373 -snap {("DDS_inst" 5)}
-wvSetCursor -win $_nWave2 484068.724265 -snap {("DDS_inst" 5)}
-wvSetCursor -win $_nWave2 135484.776552 -snap {("DDS_inst" 5)}
-wvSetMarker -win $_nWave2 30.000000
+schCreateWindow -delim "." -win $_nSchema1 -scope "DDS_tb"
+schZoomOut -win $_nSchema3 -pos -1566 3092
+schZoomOut -win $_nSchema3 -pos -1566 3092
+schZoomIn -win $_nSchema3 -pos -1543 3138
+srcHBSelect "DDS_tb.DDS_inst" -win $_nTrace1
+srcSetScope -win $_nTrace1 "DDS_tb.DDS_inst" -delim "."
+srcHBSelect "DDS_tb.DDS_inst" -win $_nTrace1
+schCreateWindow -delim "." -win $_nSchema1 -scope "DDS_tb.DDS_inst"
+verdiDockWidgetSetCurTab -dock windowDock_nSchema_3
+verdiDockWidgetSetCurTab -dock windowDock_nSchema_4
+srcHBSelect "DDS_tb.DDS_inst" -win $_nTrace1
+schCreateWindow -hierFSM -win $_nSchema4 -mode all
+verdiDockWidgetMaximize -dock windowDock_nSchema_4
+schSelect -win $_nSchema4 -inst "DDS\(@1\):Always0:27:39:RegCombo"
+schPushViewIn -win $_nSchema4
+srcSelect -win $_nTrace1 -range {27 39 1 2 1 1}
+schSelect -win $_nSchema4 -inst "DDS\(@1\):Always0:27:39:RegCombo"
+schPushViewIn -win $_nSchema4
+srcSelect -win $_nTrace1 -range {27 39 1 2 1 1}
+verdiDockWidgetRestore -dock windowDock_nSchema_4
+schSelect -win $_nSchema4 -inst "DDS\(@1\):Always0:27:39:RegCombo"
+schPushViewIn -win $_nSchema4
+srcSelect -win $_nTrace1 -range {27 39 1 2 1 1}
+srcDeselectAll -win $_nTrace1
+srcDeselectAll -win $_nTrace1
+srcSelect -win $_nTrace1 -range {32 38 1 2 4 3} -backward
+srcDeselectAll -win $_nTrace1
+srcDeselectAll -win $_nTrace1
+srcSelect -win $_nTrace1 -range {34 39 3 1 1 1} -backward
+srcDeselectAll -win $_nTrace1
+srcDeselectAll -win $_nTrace1
+srcDeselectAll -win $_nTrace1
+srcDeselectAll -win $_nTrace1
 debExit
